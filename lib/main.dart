23d5eb6cb7e5db4c179683df926d258be632cea6
Cameron Lares <cameronlares@gmail.com>
2021-07-25 16:56:55 -0700

    Created name and level attribute

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,43 @@ class NinjaCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Name',
+              'NAME',
               style: TextStyle(
                 color: Colors.grey,
-                letterSpacing: 2,
+                letterSpacing: 2.0,
               ),
             ),
+            SizedBox(height: 10.0), //Adds space between widgets
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Current Ninja Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0), //Adds space between widgets
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ), //Adds space between widgets
           ],
         ),
       ),
