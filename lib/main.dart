ec6daf219a1f32f24a8657528f468cf7485850ca
Cameron Lares <cameronlares@gmail.com>
2021-07-25 18:01:43 -0700

    Created Floating Action Button, with a Setstate to update levels

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +19,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -55,7 +71,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0), //Adds space between widgets
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
